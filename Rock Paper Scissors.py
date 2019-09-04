9527433e8ef75f708032c3bfeed7b2c10a302212
Philip Mottershead <philipmottershead@gmail.com>
2019-09-05 00:23:25 +0100

    Tidied changes

--- Rock Paper Scissors.py
+++ Rock Paper Scissors.py
@@ -10,13 +10,12 @@
 # Paper beats rock
 
 import random
+
 rock = "Rock"
 paper = "Paper"
 scissors = "Scissors"
-p1_win = "player 1"
-p2_win = "player 2"
-p1_lose = "player 1"
-p2_lose = "p2_lose"
+player_one_win = "Player One Won"
+player_two_win = "player Two Won"
 
 
 def game():
@@ -38,21 +37,16 @@ def ai_game_input():
     return random.choice([rock, paper, scissors])
 
 
-def check_result(p1, p2):
-    outcomes = {
-        (rock, scissors): "Rock breaks Scissors",
-        (scissors, paper): "Scissors cuts Paper",
-        (paper, rock): "Paper beats Rock"
-    }
-    if draw(p1, p2):
+def check_result(player_one, player_two):
+    if draw(player_one, player_two):
         print("Draw")
     else:
-        print(f"{rock_paper_scissors(p1, p2, outcomes)} Won!")
+        print(rock_paper_scissors(player_one, player_two))
 
 
 def is_new_game():
     while True:
-        input_value = input("New Game? \n")
+        input_value = input("New Game?\nY for yes\nN for N\n")
         if input_value == "Y" or input_value == "y":
             return True
         elif input_value == "N" or input_value == "n":
@@ -85,18 +79,27 @@ def user_game_input():
             print("Invalid argument")
 
 
-def draw(p1, p2):
-    if p1 == p2:
+def draw(player_one, player_two):
+    if player_one == player_two:
         return True
     else:
         return False
 
 
-def rock_paper_scissors(p1, p2,outcomes):
-    if (p1, p2) in outcomes:
-        return p1_win
-    elif (p2, p1) in outcomes:
-        return p2_win
+def rock_paper_scissors(player_one, player_two):
+    outcomes = {
+        (rock, scissors): "Rock breaks Scissors",
+        (scissors, paper): "Scissors cuts Paper",
+        (paper, rock): "Paper beats Rock"
+    }
+
+    # checks if player 1 or 2 has won
+    if (player_one, player_two) in outcomes:
+        print(outcomes.get((player_one, player_two)))
+        return player_one_win
+    elif (player_two, player_one) in outcomes:
+        print(outcomes.get((player_two, player_one)))
+        return player_two_win
 
 
 game()
