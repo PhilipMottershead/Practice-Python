ffe3710cbf5cdbb0502cb3bc4495531845376720
Philip Mottershead <philipmottershead@gmail.com>
2019-09-11 16:20:59 +0100

    Completed tutorial 17

--- Tutorials/Exercise 17 - Decode A Web Page.py
+++ Tutorials/Exercise 17 - Decode A Web Page.py
@@ -1,9 +1,17 @@
 # Use the BeautifulSoup and requests Python packages
-# print out a list of all the article titles on the New York Times homepage
+# print out a list of all the article titles on the BBC homepage
 
-# some requests code here for getting r_html
+# http://www.practicepython.org/exercise/2014/06/06/17-decode-a-web-page.html
+
 import requests
+from bs4 import BeautifulSoup
 
-url = 'http://github.com'
+url = 'https://www.bbc.co.uk/'
 r = requests.get(url)
-print(r.text)
+r_html = r.text
+
+soup = BeautifulSoup(r_html, "lxml")
+title = soup.findAll(class_="top-story__title")
+
+for item in title:
+    print(item.text.strip()+"\n")
